50b09c603bf4f70bb22f8b46a23d90b0f402c8ce
SON <SON@MSI>
2023-02-27 22:37:39 +0700

    Add project files.

---
+++ WebsiteCrawler02/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using HtmlDocument = HtmlAgilityPack.HtmlDocument;
+
+namespace WebsiteCrawler02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+         
+            HtmlDocument doc = new HtmlDocument();
+            WebClient client = new WebClient();
+            string html = client.DownloadString("https://trangvangvietnam.com/cateprovinces/200710/nh%C3%A0-h%C3%A0ng-%E1%BB%9F-t%E1%BA%A1i-tp.-h%E1%BB%93-ch%C3%AD-minh-(tphcm).html");
+            doc.OptionDefaultStreamEncoding= Encoding.UTF8;
+            Console.WriteLine(doc.DetectEncodingHtml(html));
+            doc.DetectEncodingAndLoad("C:\\Users\\SON\\Desktop\\index.html");
+            HtmlNode testNode = doc.DocumentNode.SelectNodes("//div").Where(x => x.Id == "listingsearch").First().SelectNodes("//div").Where(x => x.HasClass("boxlistings")).First();
+            String name=(testNode.SelectNodes("//h2").Where(x => x.HasClass("company_name")).First().InnerText);
+            Console.WriteLine(testNode.SelectNodes("//p").Where(x => x.HasClass("diachisection")).Last().InnerText);
+            Console.WriteLine(testNode.SelectNodes("//p").Where(x => x.HasClass("thoaisection")).First().InnerText);
+            Console.WriteLine(testNode.SelectNodes("//div").Where(x => x.HasClass("email_text")).First().ChildNodes.First().Attributes["title"].Value);
+            Console.WriteLine(testNode.SelectNodes("//a").Where(x => x.HasClass("buttonMoreDetails")).First().Attributes["href"].Value);
+            MessageBox.Show(name, "a");
+        }
+    }
+}
